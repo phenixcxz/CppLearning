f31198e603da5ecd0d53e5eb4135b038c8acfef0
cxz <cxz@MacBook.local>
2020-06-17 20:36:36 +0800

    test

---
+++ practice/day2/test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: cxz
+	> Mail: phenixcxz@163.com 
+	> Created Time: 三  6/17 20:33:04 2020
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+	cout << "hello" << endl;
+}
