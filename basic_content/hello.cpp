3e72b89319c514348f6de778c68798d5c4719314
cxz <vcx@cxzs-MacBook-Air.local>
2020-06-19 15:09:55 +0800

    test

---
+++ basic_content/hello.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: cxz
+    > Mail: phenixcxz@163.com 
+    > Created Time: 五  6/19 15:09:06 2020
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(void){
+	cout << "hello" << endl;
+	return 0;
+}
